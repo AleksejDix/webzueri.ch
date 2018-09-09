5dddfe5110989c70357c346897731c4ef7bebc92
Aleksej Dix <git@aleksejdix.com>
2018-09-09 19:03:43 +0200

    design fixes

--- pages/speakers/index.vue
+++ pages/speakers/index.vue
@@ -8,12 +8,12 @@
 
           <div class="flex-no-shrink flex items-center p-2 md:p-4">
             <div class="overflow-hidden border-2 border-white rounded-full shadow w-12 h-12 md:w-16 md:h-16 xl:w-24 xl:h-24">
-              <img class="h-full w-full bg-white align-bottom " :src="speaker.speakerPicture.url " :alt="speaker.name ">
+              <img v-if="speaker.speakerPicture" class="h-full w-full bg-white align-bottom " :src="speaker.speakerPicture.url " :alt="speaker.name ">
             </div>
           </div>
 
           <div class="flex-1 p-2 md:p-4">
-            <h2 class="text-white text-xl xl:text-3xl font-display font-bold tracking-wide mb-4 leading-tight">{{speaker.name}}</h2>
+            <h2 class="text-white text-xl xl:text-2xl font-display font-bold tracking-wide mb-4 leading-tight">{{speaker.name}}</h2>
             <div class="text-sm p-1 inline-flex items-center bg-green-light rounded-full leading-none">
               <span class="px-2 flex-1 text-green-darker font-bold">Talks</span>
               <div class="flex items-center justify-center bg-green-darker font-bold text-green-light rounded-full text-white h-6 w-6">{{speaker.talks.length}}</div>
