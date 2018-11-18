4d23218432cf2286ea126a3cda80ae661d2a1a52
Aleksej Dix <git@aleksejdix.com>
2018-11-18 09:20:06 +0100

    remove link from the talk page

--- pages/talk/_id/index.vue
+++ pages/talk/_id/index.vue
@@ -7,7 +7,7 @@
         <nuxt-link :to="{name: 'speaker-id', params: {id: speaker.id}}" class="text-on-dark-primary hover:text-blue-light no-underline inline-block" v-for="speaker in talk.speakers" v-if="talk.speakers" :key="speaker.id">
           <user-card size="xl" v-if="speaker.speakerPicture" :name="speaker.name" :photo="speaker.speakerPicture.url" />
         </nuxt-link>
-        <nuxt-link :to="{ name: 'talk-id', params: { id: talk.id }}" class="flex-1 relative block bg-primary rounded-lg p-4 text-white md:flex no-underline whitespace-normal shadow-blue">
+        <div class="flex-1 relative block bg-primary rounded-lg p-4 text-white md:flex no-underline whitespace-normal shadow-blue">
           <svg viewBox="0 0 24 8" class="w-8 h-8 absolute pin-l ml-4" style="bottom: 100%">
             <path transform="translate(0,7.3)" class="fill-blue " d="M3 8s2.021-.015 5.253-4.218C9.584 2.051 10.797 1.007 12 1c1.203-.007 2.416 1.035 3.761 2.782C19.012 8.005 21 8 21 8H3z" />
             <path transform="translate(0,8.2)" class="transition fill-primary group-hover:fill-primary-dark " d="M3 8s2.021-.015 5.253-4.218C9.584 2.051 10.797 1.007 12 1c1.203-.007 2.416 1.035 3.761 2.782C19.012 8.005 21 8 21 8H3z" />
@@ -23,7 +23,7 @@
               </Ratio>
             </div>
           </div>
-        </nuxt-link>
+        </div>
       </div>
     </div>
   </div>
